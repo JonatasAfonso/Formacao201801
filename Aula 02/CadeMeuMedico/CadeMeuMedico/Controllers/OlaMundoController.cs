e825d42ff881e2e5d9c937512f4c7ab8ec8fdc55
Jonatas Afonso <jonatas_afonso@hotmail.com>
2018-01-22 14:25:40 +0000

    Aula 02

---
+++ Aula 02/CadeMeuMedico/CadeMeuMedico/Controllers/OlaMundoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadeMeuMedico.Controllers
+{
+    public class OlaMundoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public string ObterTeste()
+        {
+            return "teste";
+        }
+    }
+}
